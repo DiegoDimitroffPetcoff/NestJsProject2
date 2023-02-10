78c5877a22c6b993863c96aa835f3cad875b3618
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-02-10 10:33:51 +0000

    Add CRUD

--- src/tuits/tuits.controller.ts
+++ src/tuits/tuits.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common';
 
 
 //EL CONTROLADOR// lo que esta entre parentecis sera la ruta mapeada
@@ -7,7 +7,7 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 export class TuitsController {
 
     //las rutas se ejecutan en el orden en el que son puestas
-
+    //GET
     //este es el decorador con el metodo GET
     @Get()
     getTuits() {
@@ -16,7 +16,7 @@ export class TuitsController {
 
     //La funcion debe ser distinta, no se debe repetir 
     //getTuits getTuit
-    @Get(':id') 
+    @Get(':id')
     getTuit(@Param() params) {
         return `Your tuit id is ${params.id}`
     }
@@ -24,13 +24,34 @@ export class TuitsController {
     //getTuit(@Param('id') id: string): string {
     //    return `Your tuit id is ${id}`
     //}
-
+    //POST
     @Post()
-    createTuit(@Body() body){
-    return body}
+    createTuit(@Body() body) {
+        return body
+    }
+    //variante
+    //@Post()
+    //createTuit(@Body('message') message: string){
+    //    return ' Your tuit was: ${message}'
+    //}
 
+    //PARA CAMBIAR EL ESTADO QUE NEST TRAE POR DEFAULD
+    @Post()
+    @HttpCode(HttpStatus.NO_CONTENT)
+    noContent(@Body('message') message: string) {
+        return `Your trui`
+    }
 
+    
+    @Patch(':id')
+    updateTuit(@Param('id') id: string, @Body() tuit): string {
+        return `Tuit ${id} has been update`
+    }
 
+    @Delete(':id')
+    deleteTuit(@Param('id') id: string): string {
+        return `Tuit ${id} has been deleted`
+    }
 
 
 
